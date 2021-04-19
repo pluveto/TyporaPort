921c12269e0b69aa6a6b4401ce6a6b411e64f9ab
Pluveto <receding@protonmail.com>
2021-04-19 22:57:33 +0800

    update

---
+++ TyporaPort/MainForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace TyporaPort
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxMarkdown = new System.Windows.Forms.TextBox();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.buttonTitleDown = new System.Windows.Forms.Button();
+            this.textBoxOutputDir = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxMarkdown
+            // 
+            this.textBoxMarkdown.Location = new System.Drawing.Point(12, 9);
+            this.textBoxMarkdown.Multiline = true;
+            this.textBoxMarkdown.Name = "textBoxMarkdown";
+            this.textBoxMarkdown.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxMarkdown.Size = new System.Drawing.Size(373, 333);
+            this.textBoxMarkdown.TabIndex = 0;
+            this.textBoxMarkdown.Text = "# text\r\n## text\r\n### text\r\n#### text";
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(400, 12);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(109, 49);
+            this.buttonConvert.TabIndex = 1;
+            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // buttonTitleDown
+            // 
+            this.buttonTitleDown.Location = new System.Drawing.Point(400, 67);
+            this.buttonTitleDown.Name = "buttonTitleDown";
+            this.buttonTitleDown.Size = new System.Drawing.Size(109, 29);
+            this.buttonTitleDown.TabIndex = 2;
+            this.buttonTitleDown.Text = "Title Down";
+            this.buttonTitleDown.UseVisualStyleBackColor = true;
+            this.buttonTitleDown.Click += new System.EventHandler(this.buttonTitleDown_Click);
+            // 
+            // textBoxOutputDir
+            // 
+            this.textBoxOutputDir.Location = new System.Drawing.Point(87, 348);
+            this.textBoxOutputDir.Name = "textBoxOutputDir";
+            this.textBoxOutputDir.Size = new System.Drawing.Size(298, 23);
+            this.textBoxOutputDir.TabIndex = 4;
+            this.textBoxOutputDir.DoubleClick += new System.EventHandler(this.textBoxOutputDir_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 351);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Output Dir";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 378);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxOutputDir);
+            this.Controls.Add(this.buttonTitleDown);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.textBoxMarkdown);
+            this.Name = "MainForm";
+            this.Text = "Output Dir";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxMarkdown;
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.Button buttonTitleDown;
+        private System.Windows.Forms.TextBox textBoxOutputDir;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TyporaPort/MainForm.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TyporaPort
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            var text = this.textBoxMarkdown.Text;
+            text = Convert(text);
+            if(null!= text)
+            {
+                this.textBoxMarkdown.Text = text;
+            }            
+        }
+        /// <summary>
+        /// 对 Typora Markdown 执行转换，转换为 Hugo Markdown。失败返回 Null
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private string Convert(string text)
+        {
+            var pos = LRMatch(text);
+            if (pos != -1)
+            {
+                MessageBox.Show("\\left 和 \\right 的数量不匹配。\n");
+                textBoxMarkdown.Select(pos, 1);
+                textBoxMarkdown.ScrollToCaret();
+                textBoxMarkdown.Focus();
+                return null;
+            }
+            text = Regex.Replace(text, @"\$\$\n*((.|\n)*?)\n*\$\$", delegate (Match match)
+            {
+                var o = match.Groups[1].Value.Trim();
+                return "\n<div>\n$$" + o + "\n$$\n</div>\n";
+            });
+
+            text = Regex.Replace(text, @"\$(.*?)\$", delegate (Match match)
+            {
+                if (match.Groups[1].Value == string.Empty)
+                {
+                    return "$$";
+                }
+                //https://xianbai.me/learn-md/article/syntax/blackslash-escapes.html
+                string o = EscapeMarkdown(match.Groups[1].Value.Trim());
+
+                return "<span>$" + o + "$</span>";
+            });
+            return text;
+        }
+
+        private static string EscapeMarkdown(string str)
+        {
+            var list = new string[]{@"\",
+                    @"`",
+                    @"*",
+                    @"_",
+                    @"{",
+                    @"}",
+                    @"[",
+                    @"]",
+                    @"(",
+                    @")",
+                    @"#",
+                    @"+",
+                    @"-",
+                    @".",
+                    @"!",};
+            var tmp = str;
+            foreach (var c in list)
+            {
+                tmp = tmp.Replace(c, "\\" + c);
+            }
+            return tmp;
+                            //.Replace("_", @"\_")
+                            //.Replace("*", @"\*")
+                            //.Replace("|", @"\|")
+                            //.Replace("\\left(", @"(")
+                            //.Replace("\\right)", @")")
+                            //.Replace("\\left[", @"[")
+                            //.Replace("\\right]", @"]")
+                            //.Replace("\\left{", @"\\{")
+                            //.Replace("\\right}", @"\\}")
+                            //.Replace("\\left", @"")
+                            //.Replace("\\right", @"");
+        }
+
+        private int LRMatch(string text)
+        {
+            var tmp =
+            text.Replace("{", "<")
+            .Replace("}", ">")
+            .Replace("\\leftarrow", "     arrow")
+            .Replace("\\rightarrow", "      arrow")
+             .Replace("\\Leftarrow", "     arrow")
+            .Replace("\\Rightarrow", "      arrow")
+            .Replace("\\left", "{    ")
+            .Replace("\\right", "     }");
+
+            int counter = 0;
+            for (int i = 0; i < tmp.Length; i++)
+            {
+                if (tmp[i] == '{')
+                {
+                    counter++;
+                }
+                if (tmp[i] == '}')
+                {
+                    counter--;
+                }
+                if (counter < 0)
+                {
+                    return i;
+                }
+            }
+            if (counter != 0)
+            {
+                return tmp.Length - 1;
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+
+        private void buttonTitleDown_Click(object sender, EventArgs e)
+        {
+            var text = this.textBoxMarkdown.Text;
+            // 特殊情况懒得处理了，不常见
+            text = "\n" + text;
+            text = text.Replace("\n## ", "\n# ");
+            text = text.Replace("\n### ", "\n## ");
+            text = text.Replace("\n#### ", "\n### ");
+            text = text.Replace("\n##### ", "\n#### ");
+            text = text.Replace("\n###### ", "\n##### ");
+            this.textBoxMarkdown.Text = text.Substring(1);
+
+        }
+
+        private void textBoxOutputDir_DoubleClick(object sender, EventArgs e)
+        {
+            Process.Start(textBoxOutputDir.Text);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            var tmp  = Application.StartupPath + "/output";
+            System.IO.Directory.CreateDirectory(tmp);
+            textBoxOutputDir.Text = tmp;
+        }
+    }
+}
